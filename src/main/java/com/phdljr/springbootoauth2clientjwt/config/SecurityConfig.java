b122d15ba49d41ee9fa08412017af5a29b88bbb3
phdljr <phdljr@naver.com>
2024-03-24 03:18:22 +0900

    Feat: edit filter sequence

--- src/main/java/com/phdljr/springbootoauth2clientjwt/config/SecurityConfig.java
+++ src/main/java/com/phdljr/springbootoauth2clientjwt/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -63,7 +64,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated());
 
         http
-            .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+            .addFilterAfter(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
 
         return http.build();
     }
