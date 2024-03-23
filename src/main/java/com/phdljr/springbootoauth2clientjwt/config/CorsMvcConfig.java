c1b21b1141d17af3350d844fea314a66f5c5fbd3
phdljr <phdljr@naver.com>
2024-03-24 03:12:20 +0900

    Feat: add cors config

--- src/main/java/com/phdljr/springbootoauth2clientjwt/config/CorsMvcConfig.java
+++ src/main/java/com/phdljr/springbootoauth2clientjwt/config/CorsMvcConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 컨트롤러단에서 보내준 데이터를 받을 수 있도록 설정
+ *
+ * 로그인의 경우 시큐리티 필터만 통과 후 응답이 되기 때문에 SecurityConfig에 설정한 CORS 값으로 진행되고,
+ * 컨트롤러를 타고 응답되는 경우 WebMvcConfigurer 설정을 통해 진행
  */
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
